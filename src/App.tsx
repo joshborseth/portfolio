d8d03ddaedab7d19a7fb916942646f647e15aaef
Joshua Borseth <joshuaborseth@gmail.com>
2022-08-29 14:25:15 -0700

    oops now text is centered for mobile users

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="h-screen w-screen flex flex-col gap-5 justify-start items-center p-20">
+    <div className="h-screen w-screen flex flex-col gap-5 justify-start items-center text-center p-20">
       <h1 className="font-bold text-5xl">Joshua Borseth</h1>
       <h2 className="font-semibold text-2xl">Check out my Projects:</h2>
       <ul>
