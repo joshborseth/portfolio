4c191fbbffc3d9f1afdb4207ee5d174f8734f223
Joshua Borseth <joshuaborseth@gmail.com>
2022-09-01 13:51:57 -0700

    added questions link and padding to outermost dic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center text-center">
+    <div className="h-screen w-screen flex flex-col justify-center items-center text-center p-10">
       <div className="flex flex-col justify-center items-center gap-10">
         <h1 className="text-7xl font-bold">Joshua Borseth</h1>
         <p className="text-2xl max-w-md">A Frontend Developer with a passion for finding practical solutions to everyday problems.</p>
@@ -10,6 +10,9 @@ const App = () => {
           </a>
           <a className="btn-accent btn-lg btn" href="https://jbmovies.joshborseth.com/">
             Movie DB Project
+          </a>
+          <a className="btn-accent btn-lg btn" href="https://questions.joshborseth.com/">
+            Questions App
           </a>
         </div>
       </div>
