676db132ef0de566462845ae1af04237a988cb06
Joshua Borseth <joshuaborseth@gmail.com>
2022-09-02 11:34:59 -0700

    added hacky fade in animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
+
 const App = () => {
+  const [fade, setFade] = useState(false);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFade(true);
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, []);
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center text-center p-10">
+    <div
+      className={`h-screen w-screen flex flex-col justify-center items-center text-center p-10 transition-opacity duration-500 ${
+        fade ? "opacity-100" : "opacity-0"
+      }`}
+    >
       <div className="flex flex-col justify-center items-center gap-10">
         <h1 className="text-7xl font-bold">Joshua Borseth</h1>
         <p className="text-2xl max-w-md">A Frontend Developer with a passion for finding practical solutions to everyday problems.</p>
